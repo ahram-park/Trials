6cb24cfe9c2cc781037d0d43af632720a0302bbd
Ahram Park <apark936@gmail.com>
2022-12-23 00:03:29 +0900

    UserControl Implementation

---
+++ MVVMToolkitTest2/MVVMToolkitTest2/Controls/LimitedInputUserControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMToolkitTest2.Controls
+{
+    /// <summary>
+    /// Interaction logic for LimitedInputUserControl.xaml
+    /// </summary>
+    public partial class LimitedInputUserControl : UserControl
+    {
+        public LimitedInputUserControl()
+        {
+            InitializeComponent();
+            //this.DataContext = this;
+            //Do not define the DataContext in the code-behind
+            //Bind the DataContext in the XAML view.
+        }
+
+        /*
+        DependencyProperty.Register("--Binding으로 연결하는 변수--",
+        typeof(--binding으로 연결된 변수의 타입--),
+        typeof(--usercontrol을 사용한 클래스의 이름--),
+        new PropertyMetadata(--기본은 null값으로--));
+        */
+
+        public string ControlText
+        {
+            get { return (string)GetValue(ControlTextProperty); }
+            set { SetValue(ControlTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ControlText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ControlTextProperty =
+            DependencyProperty.Register("ControlText", typeof(string)
+                , typeof(LimitedInputUserControl), new PropertyMetadata(String.Empty));
+
+
+        public string? Title { get; set; }
+        public int MaxLength { get; set; }
+    }
+}
--- MVVMToolkitTest2/MVVMToolkitTest2/ViewModels/MainViewModel.cs
+++ MVVMToolkitTest2/MVVMToolkitTest2/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using MVVMToolkitTest2.Messages;
 using MVVMToolkitTest2.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,20 +17,50 @@ namespace MVVMToolkitTest2.ViewModels
         public MainViewModel()
         {
             OnActivated();
-            User = new UserModel { FirstName = "Ahram", LastName = "Park", Age = 29 };
-            _name = "Ahram";
+            User = new UserModel() { FirstName = "Aaron", LastName = "Park", Age = 29 };
+            Users.Add(new UserModel() { FirstName = "Aaron", LastName = "Park", Age = 29 });
+            Users.Add(new UserModel() { FirstName = "Ahram", LastName = "Park", Age = 29 });
+            Users.Add(new UserModel() { FirstName = "SangKun", LastName = "Park", Age = 29 });
+            Users.Add(new UserModel() { FirstName = "SangHun", LastName = "Park", Age = 29 });
+            Users.Add(new UserModel() { FirstName = "Yesman", LastName = "Park", Age = 29 });
+            FirstName = "Ahram";
+            LastName = "Park";
+            
         }
 
         [ObservableProperty]
         private UserModel user;
 
         [ObservableProperty]
-        private string _name;
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        private string _firstName;
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        private string _lastName;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        [ObservableProperty]
+        private ObservableCollection<UserModel> users = new ObservableCollection<UserModel>();
+
+        [ObservableProperty]
+        private UserModel _selectedUser;
 
         [RelayCommand]
         private void ChangeFirstName(UserModel user)
         {
-            Name = user.FirstName;
+            FirstName = user.FirstName;
+        }
+
+        [RelayCommand]
+        private void AddUsers()
+        {
+            Users.Add(new UserModel() { FirstName = "Nam", LastName = "Soon", Age = 40 });
+        }
+        [RelayCommand]
+        private void RemoveUser()
+        {
+            Users.Remove(SelectedUser);
         }
 
         protected override void OnActivated()
